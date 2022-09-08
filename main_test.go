df4e6e4aa73623e91bc883635eebbbeb703a7459
VTRyo <vtryo.work@gmail.com>
2022-09-08 22:16:11 +0900

    add test

---
+++ main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetBooklogInfo(t *testing.T) {
+	loadEnv()
+	os.Setenv("ACCOUNT_ID", "vtryo")
+	account := getBooklogInfo().Tana.Account
+	name := getBooklogInfo().Tana.Name
+
+	accountExpected := "vtryo"
+	nameExpected := "Ryoの本棚"
+
+	if account != accountExpected {
+		t.Errorf("got: %v\nwant: %v", account, accountExpected)
+	}
+	if name != nameExpected {
+		t.Errorf("got: %v\nwant: %v", name, nameExpected)
+	}
+}
